192866f91c633636f589718d33d83e5a216d5ff8
nixsiow <nixsiow@hotmail.com>
2014-06-03 16:30:28 +1000

    bugs fixed

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function() {
   //open project info
-  $('#cd-team').find('ul a').on('click', function(event){
+  $('#cd-team').find('ul a').on('click', function(event) {
     event.preventDefault();
     var selected_member = $(this).data('type');
     $('main').addClass('slide-out');
-    $('.cd-member-bio.'+selected_member+'').addClass('slide-in');
+    $('.cd-member-bio.' + selected_member + '').addClass('slide-in');
     $('.cd-member-bio-close').addClass('is-visible');
     $('body').addClass('overflow-hidden');
   });
 
   //close project info
-  $(document).on('click', '.cd-overlay, .cd-member-bio-close', function(event){
+  $(document).on('click', '.cd-overlay, .cd-member-bio-close', function(event) {
     event.preventDefault();
     $('main').removeClass('slide-out');
     $('.cd-member-bio').removeClass('slide-in');
