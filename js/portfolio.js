e0a9b5bcf4cc8edfc6dfc53f59bd97bb9bf9f910
nixsiow <nixsiow@hotmail.com>
2014-06-04 16:11:07 +1000

    add group yearbook

--- js/portfolio.js
+++ js/portfolio.js
@@ -17,4 +17,26 @@ $(document).ready(function() {
     $('.cd-member-bio-close').removeClass('is-visible');
     $('body').removeClass('overflow-hidden');
   });
+
+
+  // ------------------------
+  // For Jump to Top button
+  // ------------------------
+
+  //Check to see if the window is top if not then display button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+      $('.scrollToTop').fadeIn();
+    } else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
+
+  //Click event to scroll to top
+  $('.scrollToTop').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
 });
